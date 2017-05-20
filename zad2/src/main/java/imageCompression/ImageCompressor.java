b7a9dcdc4bbfd834612d8e4c2326fa8ffba9376b
Patryk Kacperski <patryk.kacperski141095@gmail.com>
2017-05-20 18:20:35 +0200

    Ex2 finished

--- zad2/src/main/java/imageCompression/ImageCompressor.java
+++ zad2/src/main/java/imageCompression/ImageCompressor.java
@@ -1,6 +1,7 @@
 package imageCompression;
 
 import machineLearning.KMeans;
+import model.Pair;
 import model.Pixel;
 import org.la4j.Matrix;
 import org.la4j.Vector;
@@ -10,6 +11,8 @@ import util.FilesUtil;
 import util.VectorUtil;
 
 import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by patry on 01/05/17.
@@ -21,6 +24,8 @@ public class ImageCompressor {
     private int numberOfColors;
     private int[] meanColors;
     private Pixel[] initialPositions;
+    private byte[] compressedImageData;
+    private int currentIndex;
 
     public ImageCompressor(String imageName, int numberOfColors) {
         this.imageName = imageName;
@@ -32,6 +37,20 @@ public class ImageCompressor {
     public void compress() {
         computeMeanColors();
         updateColors();
+        saveCompressedImage();
+    }
+
+    private void saveCompressedImage() {
+        String filePath = "./pictures/" + imageName + "_compressedData";
+//        byte[] testData = {0x10, 0x16, 0x1a, 0x0d, 0x2c};
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+            fileOutputStream.write(compressedImageData);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void updateColors() {
@@ -40,14 +59,37 @@ public class ImageCompressor {
         BufferedImage largeImage = readLargeImage();
         int height = largeImage.getHeight();
         int width = largeImage.getWidth();
+        compressedImageData = new byte[4 + (numberOfColors * 3) + ((height * width * numberOfColors) / 256)];
+        setUpColorsDictionary(height, width);
         for (int i = 0; i < height; ++i) {
             for (int j = 0; j < width; ++j) {
                 int color = largeImage.getRGB(j, i);
-                int newColor = findNewColor(color);
-                largeImage.setRGB(j, i, newColor);
+                Pair<Integer, Byte> newColor = findNewColor(color);
+                largeImage.setRGB(j, i, newColor.getFirst());
+                compressedImageData[currentIndex++] = newColor.getSecond();
             }
         }
         saveLargeImage(largeImage);
+    }
+
+    private void setUpColorsDictionary(int height, int width) {
+        byte height1 = (byte) ((height & 0x0000ff00) >> 8);
+        byte height2 = (byte) (height & 0x000000ff);
+        byte width1 = (byte) ((width & 0x0000ff00) >> 8);
+        byte width2 = (byte) (width & 0x000000ff);
+        compressedImageData[0] = height1;
+        compressedImageData[1] = height2;
+        compressedImageData[2] = width1;
+        compressedImageData[3] = width2;
+        for (int i = 0; i < numberOfColors; i++) {
+            byte red = (byte) ((meanColors[i] >> 16) & 0x000000FF);
+            byte green = (byte) ((meanColors[i] >> 8) & 0x000000FF);
+            byte blue = (byte) (meanColors[i] & 0x000000FF);
+            compressedImageData[4 + i * 3] = red;
+            compressedImageData[4 + i * 3 + 1] = green;
+            compressedImageData[4 + i * 3 + 2] = blue;
+        }
+        currentIndex = numberOfColors * 3 + 4;
     }
 
     private void saveColors() {
@@ -62,8 +104,9 @@ public class ImageCompressor {
         ImageSaver.getInstance().saveImage(largeImage, path);
     }
 
-    private int findNewColor(int color) {
+    private Pair<Integer, Byte> findNewColor(int color) {
         int newColor = meanColors[0];
+        byte newColorIndex = 0;
         for (int i = 1; i < meanColors.length; ++i) {
 //            if (meanColors[i] != 0 && Math.abs(color - meanColors[i]) < Math.abs(color - newColor)) {
             int red = (color >> 16) & 0x000000FF;
@@ -84,9 +127,10 @@ public class ImageCompressor {
             if (VectorUtil.getInstance().euclideanDistance(colorVector, meanColorVector)
                     < VectorUtil.getInstance().euclideanDistance(colorVector, newColorVector)) {
                 newColor = meanColors[i];
+                newColorIndex = (byte) i;
             }
         }
-        return newColor;
+        return new Pair<>(newColor, newColorIndex);
     }
 
     private void computeMeanColors() {
@@ -100,7 +144,7 @@ public class ImageCompressor {
     private void initKMeans(BufferedImage image, Pixel[][] pixels) {
         Matrix dataset = createDataset(image, pixels);
 //        Matrix centroids = getInitialCentroidPosiions(image);
-        kMeans = new KMeans(30, dataset, numberOfColors);
+        kMeans = new KMeans(20, dataset, numberOfColors);
     }
 
     private Matrix createDataset(BufferedImage image, Pixel[][] pixels) {
